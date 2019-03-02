4c37c2a6267dd8cba8033c4122b927171e97349f
1405918 <1405918@brunel.ac.uk>
2019-03-02 17:03:24 +0000

    added an extra class for better readability

---
+++ Treatwell/src/Box.java
@@ -0,0 +1,21 @@
+
+public class Box {
+
+	
+	
+public static void main(String[] args) {
+		
+		 
+		Test a = new Test(4,4); // calling the class Test and inputs the numbers to draw the box width by height
+		a.Draw(); //draws the square using ascii characters
+		
+		
+	//Test.GridExample(); //calling the grid method 
+		
+		
+
+	}
+	
+	
+	
+}
--- Treatwell/src/Test.java
+++ Treatwell/src/Test.java
@@ -1,43 +1,85 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		GridExample(); //calling the grid method 
-		
-		
-
-	}
-
-	
-	public static void GridExample() {
-		
-		StdDraw.setScale(0,5); // setting the scale of the grid, 0,5 means it will fit a 5 by 5
-
-		int[][] GridSize = new int [4][4]; //enter grid size here
-
-		for (int x = 0; x < GridSize.length; x++) 
-		{
-		  for (int y = 0; y < GridSize.length; y++)
-		  {
-		   // GridSize[x][y] =255;
-		  }
-		}
-
-		for (int x = 0; x < GridSize.length; x++)
-		{
-		  for (int y = 0; y <GridSize.length; y++)
-		  {
-		    StdDraw.square(x, y, 1); //drawing the scale according to the input of grid size 
-		  }
-		}
-		
-		
-		
-	}
+	private int width; //private because these instances are only called in this class
+	private int height;
 	
 	
 	
+public Test(int w, int h) { //this method is stating what type of variables they are
+		
 	
+		width = w; 
+		height = h; 
 	
 }
+
+	public void Draw() {
+		
+		
+		for(int i = 0; i < width; i++) { //flow is for the amount of hyphens for the top width
+			System.out.print("-"); //width top
+			System.out.print("  "); //added spacing to draw a square
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < height; i++) { //this for loop is for the left side of height
+			System.out.print("|"); // height left
+			
+			for(int j = 0; j < width; j++) { //this loop is for the right side of height
+				System.out.print("  "); //this for loop is for the general width 
+				
+			}
+			
+			System.out.print("|"); //right side of height 
+			System.out.println();
+			
+		}
+		
+		for(int i = 0; i < width; i++) {
+			System.out.print("-");  //width bottom 
+			System.out.print("  "); //space between the dashes
+		}
+	
+	}
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+//	public static void GridExample() {
+//		
+//		StdDraw.setScale(0,4); // setting the scale of the grid, 0,5 means it will fit a 5 by 5
+//
+//		int[][] GridSize = new int [4][4]; //enter grid size here
+//
+//		for (int x = 0; x < GridSize.length; x++) 
+//		{
+//		  for (int y = 0; y < GridSize.length; y++)
+//		  {
+//		   // GridSize[x][y] =255;
+//		  }
+//		}
+//
+//		for (int x = 0; x < GridSize.length; x++)
+//		{
+//		  for (int y = 0; y <GridSize.length; y++)
+//		  {
+//		    StdDraw.square(x, y, 1); //drawing the scale according to the input of grid size 
+//		  }
+//		}
+//		
+//		
+//		
+//	}
+	
+	
+	
+	
+	
+
