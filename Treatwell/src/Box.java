55a9886cc2c6f23f89f652194b1561aaba22ae8b
1405918 <1405918@brunel.ac.uk>
2019-03-03 13:23:15 +0000

    Renamed the classes

--- Treatwell/src/Box.java
+++ Treatwell/src/Box.java
@@ -6,11 +6,11 @@ public class Box {
 public static void main(String[] args) {
 		
 		 
-		Test a = new Test(4,4); // calling the class Test and inputs the numbers to draw the box width by height
-		a.Draw(); //draws the square using ascii characters
+		DrawBox a = new DrawBox(4,4); // calling the class Test and inputs the numbers to draw the box width by height
+		a.Draw(); //draws the square using ascii characters which are "-" and "|"
 		
 		
-	//Test.GridExample(); //calling the grid method 
+	
 		
 		
 
--- Treatwell/src/DrawBox.java
+++ Treatwell/src/DrawBox.java
@@ -1,12 +1,12 @@
 
-public class Test {
+public class DrawBox {
 
 	private int width; //private because these instances are only called in this class
 	private int height;
 	
 	
 	
-public Test(int w, int h) { //this method is stating what type of variables they are
+public DrawBox(int w, int h) { //this method is stating what type of variables they are
 		
 	
 		width = w; 
@@ -52,32 +52,7 @@ public Test(int w, int h) { //this method is stating what type of variables they
 	
 	
 	
-//	public static void GridExample() {
-//		
-//		StdDraw.setScale(0,4); // setting the scale of the grid, 0,5 means it will fit a 5 by 5
-//
-//		int[][] GridSize = new int [4][4]; //enter grid size here
-//
-//		for (int x = 0; x < GridSize.length; x++) 
-//		{
-//		  for (int y = 0; y < GridSize.length; y++)
-//		  {
-//		   // GridSize[x][y] =255;
-//		  }
-//		}
-//
-//		for (int x = 0; x < GridSize.length; x++)
-//		{
-//		  for (int y = 0; y <GridSize.length; y++)
-//		  {
-//		    StdDraw.square(x, y, 1); //drawing the scale according to the input of grid size 
-//		  }
-//		}
-//		
-//		
-//		
-//	}
-	
+
 	
 	
 	
