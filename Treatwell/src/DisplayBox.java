9753b88cc4a259c2a70777e6ad6c009971881370
Darren Jayasinghe <darrenjayasinghe@Darren-Js-MacBook-Pro.local>
2020-06-23 10:06:42 +0100

    test test

--- Treatwell/src/DisplayBox.java
+++ Treatwell/src/DisplayBox.java
@@ -1,21 +1,21 @@
 
 public class DisplayBox {
 
-	
-	
+
+
 public static void main(String[] args) {
-		
-		 
+
+
 		DrawBox a = new DrawBox(4,4); // calling the class Test and inputs the numbers to draw the box width by height
 		a.Draw(); //draws the square using ascii characters which are "-" and "|"
-		
-		
-	
-		
+
+		//testing to see if it pushes
+
 		
 
+
 	}
-	
-	
-	
+
+
+
 }
